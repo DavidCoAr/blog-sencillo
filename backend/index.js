4ed5518d6f000266aeb892cf421189a339c1b76c
DavidCoAr <127534314+DavidCoAr@users.noreply.github.com>
2023-07-01 09:38:21 +0200

    Creación Endpoints
    -obtener todos los posts:
    app.get('/allposts',…), consulta SELECT
    
    -crear un nuevo post:
    app.post('/newpost',…), consulta INSERT
    
    -actualizar un post:
    app.put('/posts/:id_entrada'…), consulta UPDATE
    
    -borrar un post (NO BORRA LA IMAGEN DEL SERVIDOR):
    app.delete('/posts/:id_entrada',consulta DELETE

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,100 @@ app.use(cors());
 //Necesario para trabajar con fecha/hora de una manera sencilla
 const moment = require("moment");
 
+
+//CREACIÓN DE ENDPOINTS
+
+//01- Definir el endpoint para obtener todos los posts
+app.get('/allposts', async (req, res) => {
+    try {
+      // Ejecutar una consulta SELECT para seleccionar todos los campos de la tabla "posts" en orden descendente de id_entrada, así muestro 1º la entrada más nueva en el feed
+      const posts = await sequelize.query('SELECT * FROM posts ORDER BY id_entrada DESC', { type: sequelize.QueryTypes.SELECT });
+  
+      // Responder con los posts encontrados
+      res.json(posts);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error interno del servidor');
+    }
+  });
+
+//02- Definir el endpoint para crear un nuevo post
+app.post('/newpost', async (req, res) => {
+    try {
+      // Obtener los datos del nuevo post desde el cuerpo de la solicitud
+      const { img_entrada, encabezado, fecha_publicacion, contenido } = req.body;
+  
+      // Ejecutar una consulta INSERT para crear un nuevo registro en la tabla "posts"
+      await sequelize.query(
+        `INSERT INTO posts (img_entrada, encabezado, fecha_publicacion, contenido)
+         VALUES (?, ?, ?, ?)`,
+        {
+          replacements: [img_entrada, encabezado, fecha_publicacion, contenido],
+          type: sequelize.QueryTypes.INSERT
+        }
+      );
+  
+      // Enviar una confirmación de éxito como respuesta
+      res.json({ message: "Post creado exitosamente" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error interno del servidor');
+    }
+  });
+  
+//03- Definir el endpoint para editar un post existente
+app.put('/post/:id_entrada', async (req, res) => {
+    try {
+      // Obtener el ID del post a editar de los parámetros de la ruta
+      const id_entrada = req.params.id_entrada;
+  
+      // Obtener los datos actualizados del post desde el cuerpo de la solicitud
+      const { img_entrada, encabezado, fecha_publicacion, contenido } = req.body;
+  
+      // Ejecutar una consulta UPDATE para editar el registro en la tabla "posts"
+      const updatePost = await sequelize.query(
+        `UPDATE posts
+         SET img_entrada = ?, encabezado = ?, fecha_publicacion = ?, contenido = ?
+         WHERE id_entrada = ?`,
+        {
+          replacements: [img_entrada, encabezado, fecha_publicacion, contenido, id_entrada],
+          type: sequelize.QueryTypes.UPDATE
+        }
+      );
+  
+      // Enviar una confirmación de éxito como respuesta
+      res.json({ message: "Post editado exitosamente" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error interno del servidor');
+    }
+  });
+  
+//04- Definir el endpoint para eliminar un post
+app.delete('/posts/:id_entrada', async (req, res) => {
+  try {
+    // Obtener el ID del post a eliminar de los parámetros de la ruta
+    const id_entrada = req.params.id_entrada;
+
+    // Ejecutar una consulta DELETE para eliminar el registro de la tabla "posts"
+    const deletePost = await sequelize.query(
+      `DELETE FROM posts
+       WHERE id_entrada = ?`,
+      {
+        replacements: [id_entrada],
+        type: sequelize.QueryTypes.DELETE
+      }
+    );
+
+    // Enviar una confirmación de éxito como respuesta
+    res.json({ message: "Post eliminado exitosamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error interno del servidor');
+  }
+});
+
+  
 //Iniciar el servidor tras en el puerto 3000 
 app.listen(3000, function(){
     console.log("Sistema funcionando en el puerto 3000");
