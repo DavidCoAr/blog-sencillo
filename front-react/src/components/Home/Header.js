097a35c108f0f6712a0c43a51c9eb39f0dc8dcbf
DavidCoAr <127534314+DavidCoAr@users.noreply.github.com>
2023-07-03 19:54:29 +0200

    Revisión estructura del componente Header

--- front-react/src/components/Home/Header.js
+++ front-react/src/components/Home/Header.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="container">
+    <header  className="container">
       <div className="row justify-content-center">
         <div className="col-12">
           <h1 className="text-center">FINAL FANTASY Y AMIGOS</h1>
         </div>
       </div>
-    </div>
+    </header >
   );
 }
 
